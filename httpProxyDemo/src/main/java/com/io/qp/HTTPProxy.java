274ebf18666b22215b59b7a6e1b47945284c6e57
AsGz <zhangqp1983@gmail.com>
2017-05-27 11:38:41 +0800

    add print

--- httpProxyDemo/src/main/java/com/io/qp/HTTPProxy.java
+++ httpProxyDemo/src/main/java/com/io/qp/HTTPProxy.java
@@ -1,7 +1,9 @@
 package com.io.qp;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.ext.web.Router;
 
 /**
@@ -14,10 +16,35 @@ public class HTTPProxy extends AbstractVerticle {
         HttpServer server = vertx.createHttpServer();
         Router router = Router.router(vertx);
         router.route().handler(routingContext -> {
-            //check auth
+            
+        	System.out.println("===================================");
+        	//check auth
+        	HttpServerRequest r = routingContext.request();
+        	MultiMap headerMap = r.headers();
+        	MultiMap paramsMap = r.params();
+        	String path = r.path();
             //split stream
             //upstream
             //response
+        	System.out.println("headers:");
+        	headerMap.forEach(header ->{
+        		System.out.println("\t" + header.getKey() + "\t" + header.getValue());
+        	});
+        	
+        	System.out.println("params:");
+        	paramsMap.forEach(p ->{
+        		System.out.println("\t" + p.getKey() + "\t" + p.getValue());
+        	});
+   
+        	System.out.println("cookies:");
+        	routingContext.cookies().forEach(cookie->{
+        		//cookie.getDomain()
+        		System.out.println("\t" + cookie.getName() + "\t" + cookie.getValue());
+        	});
+        	
+        	System.out.println(path);
+        	
+        	routingContext.response().end("hello, proxy!");
         });
         //TODO: need read from config
         server.requestHandler(router::accept).listen(8081, "127.0.0.1");
